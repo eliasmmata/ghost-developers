a26a1ba685541c5489bc3b39f1714b47e36227c2
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2021-12-02 21:32:08 +0100

    trying put name in secret

--- public/build/test.66f10dba.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[43],{1545:(e,s,t)=>{t(1545);alert("test")}},e=>{var s;s=1545,e(e.s=s)}]);
---
+++ public/build/test.js
@@ -0,0 +1,23 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["test"],{
+
+/***/ "./assets/js/test.js":
+/*!***************************!*\
+  !*** ./assets/js/test.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _js_test_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../js/test.js */ "./assets/js/test.js");
+
+alert('test');
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./assets/js/test.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7OztBQUFBO0FBRUFBLEtBQUssQ0FBQyxNQUFELENBQUwiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hc3NldHMvanMvdGVzdC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgJy4uL2pzL3Rlc3QuanMnO1xuXG5hbGVydCgndGVzdCcpOyJdLCJuYW1lcyI6WyJhbGVydCJdLCJzb3VyY2VSb290IjoiIn0=
